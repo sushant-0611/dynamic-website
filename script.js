5cfc1d36e8effaf64f81c6dd161dbca5e1ddd76b
Sushant Kakade <sushantkakade1111@gmail.com>
2026-01-09 21:14:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const hour = new Date().getHours();
+let message;
+
+if (hour < 12) {
+  message = "Good Morning ☀️";
+} else if (hour < 18) {
+  message = "Good Afternoon 🌤️";
+} else {
+  message = "Good Evening 🌙";
+}
+
+document.getElementById("greeting").innerText = message;
